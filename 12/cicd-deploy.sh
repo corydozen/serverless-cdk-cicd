059ca83b8a711bdee6071140f6d15db1087f8888
Cory Martin <cory.martin@dozensoft.com>
2019-10-14 15:32:17 -0500

    bug fixes

--- 12/cicd-deploy.sh
+++ 12/cicd-deploy.sh
@@ -11,11 +11,11 @@ cdk deploy TodosTestingAppsync --require-approval never &> ../cdkdeployresult_ap
 wait $!
 cdk deploy TodosTestingCognitoIam --require-approval never
 cd ..
-echo "export default {};" > src/config.ts
+echo "export default {};" > src/config.js
 node parseAwsOutputs.js src/config.js
 chmod +x setAwsResourcesEnvVars.sh
 . ./setAwsResourcesEnvVars.sh
-aws cognito-idp sign-up --region us-east-1 --client-id $WEBCLIENTID --username admin@example.com --password Passw0rd!  --user-attributes '[{"Name":"custom:first_name","Value":"Admin"},{"Name":"custom:last_name","Value":"Istrator"},{"Name":"custom:organization","Value":"Example"}]'
+aws cognito-idp sign-up --region us-east-1 --client-id $WEBCLIENTID --username admin@example.com --password Passw0rd!  --user-attributes '[{"Name":"custom:first_name","Value":"Admin"},{"Name":"custom:last_name","Value":"Istrator"}]'
 aws cognito-idp admin-confirm-sign-up --region us-east-1 --user-pool-id $USERPOOLID --username admin@example.com
 npm i
 node getUserJwt.js admin@example.com Passw0rd! $USERPOOLID $WEBCLIENTID $APIURL
